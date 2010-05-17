8d08df47d276fdbcd13e9c63bcc28ac7bcc4871c
goldbook <hoge>
2010-05-18 04:17:02 +0900

    first commit

---
+++ src/goldbook/com/twitter/SqlClient.java
@@ -0,0 +1,113 @@
+package goldbook.com.twitter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class SqlClient extends SQLiteOpenHelper {
+
+	private static final String HITOKUCHI = "http://feeds.feedburner.com/hitokuchi_";
+	private static final String TENKO2_SQL_CLIENT = "tenko2.SqlClient";
+	private static final String AREA_SEARCH_SQL = "select url.num from yohoku, url "
+			+ "where yohoku.府県予報区=url.AdminArea and yohoku.一次細分区域=url.primDivArea "
+			+ "and 府県予報区 = ? and (区域 = ? or 区域 = ?) "
+			+ "and (限定 = ? or 区域除外 not like ? or (限定 is NULL and 区域除外 is NULL) ) limit 1";
+	private Context RootContext;
+
+	private String url;
+	private String name;
+	private boolean isDatabaseExist = true;
+
+	/**
+	 * @return urlNum
+	 */
+	public String getUrlNum() {
+		return url;
+	}
+
+	public SqlClient(Context context, String name) {
+		super(context, name, null, 1);
+		RootContext = context;
+		this.name = name;
+	}
+
+	/**
+	 * @param adminArea
+	 * @param SubAdminArea
+	 * @param locality
+	 * @throws IOException
+	 */
+	public void search(String adminArea, String SubAdminArea, String locality)
+			throws IOException {
+		SQLiteDatabase db = getReadableDatabase();
+
+		if (!isDatabaseExist) {
+			createDatabase();
+			db.close();
+			db = getReadableDatabase();
+		}
+
+		String[] areaNames = new String[] { adminArea, SubAdminArea, locality,
+				locality, "%" + locality + "%" };
+		Cursor c = db.rawQuery(AREA_SEARCH_SQL, areaNames);
+		c.moveToFirst(); // 最初のカラムへ移動
+		url = HITOKUCHI + c.getInt(c.getColumnIndex("num"));
+	}
+
+	/*
+	 * (非 Javadoc)
+	 *
+	 * @see
+	 * android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite
+	 * .SQLiteDatabase, int, int)
+	 */
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	}
+
+	private void createDatabase() throws IOException {
+		Log.d(TENKO2_SQL_CLIENT, "onCreate!");
+		AssetManager manager = RootContext.getResources().getAssets();
+
+		File pasteFile = new File("/data/data/" + RootContext.getPackageName()
+				+ "/databases/", name);
+		pasteFile.mkdirs();
+
+		pasteFile.createNewFile();
+
+		// ストリーム準備
+		InputStream input = manager.open(name); // コピー元
+		// ZipInputStream zipInput = new ZipInputStream(input);
+		OutputStream output = new FileOutputStream(pasteFile);
+
+		// コピー貼付け作業開始
+		Log.d(TENKO2_SQL_CLIENT, "start Extract ZipFile");
+
+		int DEFAULT_BUFFER_SIZE = 1024 * 4;
+		byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+
+		int n = 0;
+		while (-1 != (n = input.read(buffer))) {
+			output.write(buffer, 0, n);
+		}
+
+		// ストリームを閉じる
+		input.close();
+		output.close();
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// onCreateでデータ投入を行うと失敗したのでフラグのみ立てる
+		isDatabaseExist = false;
+	}
+}
---
+++ src/goldbook/com/twitter/tenko2.java
@@ -0,0 +1,93 @@
+package goldbook.com.twitter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.text.util.Linkify;
+import android.widget.TextView;
+
+public class tenko2 extends Activity implements LocationListener {
+	private static final String YOHOKU = "yohoku.sqlite";
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// 状況から最適な位置情報サービスを選択し、最近取得した位置情報を取得
+		LocationManager manager = (LocationManager) this
+				.getSystemService(tenko2.LOCATION_SERVICE);
+		boolean enableOnly = true;
+		String bestProvider = manager.getBestProvider(new Criteria(),
+				enableOnly);
+
+		manager.requestLocationUpdates(bestProvider, 1, 1, this);
+		Location location = manager.getLastKnownLocation(bestProvider);
+
+		// ジオコーダに渡して住所を取得
+		Geocoder geocoder = new Geocoder(this, Locale.JAPAN);
+		try {
+			List<Address> list = geocoder.getFromLocation(location
+					.getLatitude(), location.getLongitude(), 5);
+
+			Address address = list.get(0);
+			int i = 0;
+			while((address = list.get(i++)).getLocality() == null){
+
+			}
+
+			// sqlite検索クラスに渡してひとくち予報のURL末尾番号を得る
+			SqlClient client = new SqlClient(this, YOHOKU);
+			String subAdminArea = address.getSubAdminArea();
+			if (subAdminArea == null) {
+				subAdminArea = "hoge";
+			}
+			client.search(address.getAdminArea(), subAdminArea, address
+					.getLocality());
+
+			// ひとくち予報のURLを表示
+			TextView tv = new TextView(this);
+			tv.setText(client.getUrlNum());
+			Linkify.addLinks(tv, Linkify.ALL);
+			tv.setLinksClickable(true);
+
+			setContentView(tv);
+
+			// 位置取得終了
+			manager.removeUpdates(this);
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+
+	public void onLocationChanged(Location location) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	public void onProviderDisabled(String s) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	public void onProviderEnabled(String s) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	public void onStatusChanged(String s, int i, Bundle bundle) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+}
